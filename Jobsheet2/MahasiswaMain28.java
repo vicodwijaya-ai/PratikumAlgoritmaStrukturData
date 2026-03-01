b25612a40b241959d7aaceefe062f01140dc0d2b
vicodwijaya-ai <vicodwijaya@gmail.com>
2026-03-01 14:23:47 +0700

    penambahan konstruktor

--- Jobsheet2/MahasiswaMain28.java
+++ Jobsheet2/MahasiswaMain28.java
@@ -4,11 +4,16 @@ public class MahasiswaMain28 {
         Mahasiswa28 mhs1 = new Mahasiswa28();
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "224172171";
+        mhs1.kelas = "SI 2J";
         mhs1.ipk = 3.55;
         
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa28 mhs2 = new Mahasiswa28 ("Annisa Nanila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
